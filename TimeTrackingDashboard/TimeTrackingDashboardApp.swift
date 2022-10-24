0e00c1d09bfd73180a5dd7a72611535800d8843c
Bonnie Why <bonnie.why@sherwin.com>
2022-10-24 11:25:27 -0500

    initial commit

---
+++ TimeTrackingDashboard/TimeTrackingDashboardApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimeTrackingDashboardApp.swift
+//  TimeTrackingDashboard
+//
+//  Created by Bonnie Why on 10/24/22.
+//
+
+import SwiftUI
+
+@main
+struct TimeTrackingDashboardApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
