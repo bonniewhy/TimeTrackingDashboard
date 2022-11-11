a8467b96ac154ca7b0595f9a314378d893211e08
Bonnie Why <bonnie.why@sherwin.com>
2022-11-11 12:10:04 -0600

    Adding foundational style guide elements

---
+++ TimeTrackingDashboard/Styles/Color.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+public extension Color {
+    static let blueDark = Color("Blue - Dark")
+    static let blueDesaturated = Color("Blue - Desaturated")
+    static let bluePale = Color("Blue - Pale")
+    static let blueSoft = Color("Blue - Soft")
+    static let blueVeryDark = Color("Blue - Very Dark")
+    static let blue = Color("Blue")
+    static let gold = Color("Gold")
+    static let limeGreen = Color("Lime Green")
+    static let orange = Color("Orange")
+    static let redLight = Color("Red - Light")
+    static let violet = Color("Violet")
+}
---
+++ TimeTrackingDashboard/Styles/Typography/Typography.swift
@@ -0,0 +1,8 @@
+import SwiftUI
+
+public extension Font {
+    static var headingLarge: Font { Font.custom("Rubik-Regular", size: 18.0) }
+    static var headingSmall: Font { Font.custom("Rubik-Bold", size: 14.0) }
+    static var bodyCopyLarge: Font { Font.custom("Rubik-Regular", size: 14.0) }
+    static var bodyCopySmall: Font { Font.custom("Rubik-Regular", size: 12.0) }
+}
